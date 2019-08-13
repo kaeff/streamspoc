06601caa6e667f849a954fc853e6adf350f80bc9
kaeff <hi@kaeff.net>
2019-08-13 15:37:47 +0200

    Add consumer with correct serdes

---
+++ bin/kafka-console-consumer-wordcount.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+docker-compose exec kafka kafka-console-consumer --bootstrap-server localhost:9092 \
+        --topic streams-wordcount-output \
+        --from-beginning \
+        --formatter kafka.tools.DefaultMessageFormatter \
+        --property print.key=true \
+        --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer \
+        --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer
